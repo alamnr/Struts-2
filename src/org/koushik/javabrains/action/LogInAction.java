903b6fbdd42647234c168c9b3881f7fce967a787
alamnr <alamnr@gmail.com>
2018-05-08 21:21:20 +0600

    Commit for Struts2_Tutorial-15-Using_Model_Object

--- src/org/koushik/javabrains/action/LogInAction.java
+++ src/org/koushik/javabrains/action/LogInAction.java
@@ -1,24 +1,45 @@
 package org.koushik.javabrains.action;
 
 import org.apache.commons.lang3.StringUtils;
+import org.koushik.javabrains.model.User;
+import org.koushik.javabrains.service.LoginService;
 
 import com.opensymphony.xwork2.Action;
 import com.opensymphony.xwork2.ActionSupport;
+import com.opensymphony.xwork2.ModelDriven;
 import com.sun.net.httpserver.Authenticator.Success;
 
 
-public class LogInAction  extends ActionSupport {
+public class LogInAction  extends ActionSupport  implements ModelDriven<User>{
 	
-	private String userId;
-	private String password;
+	/*private String userId;
+	private String password;*/
+	
+	//private User user ;
+	
+	private User user = new User();
 	
 	// BP-1: not use hard codded return string type instead use constant
 	// or implement Action interface
 	
 	//private static String SUCCESS="success";
 	
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
 	public String execute(){
-		if(getUserId().equals("userId") && getPassword().equals("password")){
+		
+		LoginService loginService = new LoginService();
+		/* user = new User();
+		user.setUserId(getUserId());
+		user.setPassword(getPassword());*/
+		
+		if(loginService.verifyLogin(user)){
 			//return "success";
 			return SUCCESS;
 				
@@ -29,18 +50,24 @@ public class LogInAction  extends ActionSupport {
 	}
 	
 	public void validate(){
-		if (StringUtils.isEmpty(getUserId())){
+		if (StringUtils.isEmpty(user.getUserId())){
 			// user id blank
 			addFieldError("userId", "User ID can't be blank");
 		}
 		
-		if (StringUtils.isEmpty(getPassword())){
+		if (StringUtils.isEmpty(user.getPassword())){
 			// password blank
 			addFieldError("password", "Password can't be blank");
 		}
 	}
 
-	public String getUserId() {
+	@Override
+	public User getModel() {
+		
+		return user;
+	}
+
+	/*public String getUserId() {
 		return userId;
 	}
 
@@ -55,5 +82,5 @@ public class LogInAction  extends ActionSupport {
 	public void setPassword(String password) {
 		this.password = password;
 	}
-
+*/
 }
