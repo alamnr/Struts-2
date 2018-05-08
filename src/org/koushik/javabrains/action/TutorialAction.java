4e392d1014a0bf59e407b7018f9d4800db64e8b9
alamnr <alamnr@gmail.com>
2018-05-08 16:43:16 +0600

    Commit for Struts2-Tutorial06

--- src/org/koushik/javabrains/action/TutorialAction.java
+++ src/org/koushik/javabrains/action/TutorialAction.java
@@ -1,10 +1,27 @@
 package org.koushik.javabrains.action;
 
+import org.koushik.javabrains.service.TutorialFinderService;
+
 public class TutorialAction {
 	
+	private String bestTutorialSite;
+	
 	public String execute(){
-		System.out.println("Hello  from execute");
+		//System.out.println("Hello  from execute");
+		// Better to have interface and the inject that service bean through spring
+		TutorialFinderService tutorialFinderService =  new TutorialFinderService();
+		setBestTutorialSite(tutorialFinderService.getBestTutorialSite());
 		return "success";
 	}
+
+	public String getBestTutorialSite() {
+		return bestTutorialSite;
+	}
+
+	public void setBestTutorialSite(String bestTutorialSite) {
+		this.bestTutorialSite = bestTutorialSite;
+	}
+	
+	
 
 }
