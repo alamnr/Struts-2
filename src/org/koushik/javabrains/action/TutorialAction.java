8ac8fc65d6d724182ae1f7f17448bf90511f0435
alamnr <alamnr@gmail.com>
2018-05-08 17:07:40 +0600

    Commit for Struts2_Tutorial-08-Accessing_Input_Parameters

--- src/org/koushik/javabrains/action/TutorialAction.java
+++ src/org/koushik/javabrains/action/TutorialAction.java
@@ -6,11 +6,16 @@ public class TutorialAction {
 	
 	private String bestTutorialSite;
 	
+	// Struts Interceptor transfer the input parameters to  action's member variables
+	// and the action instance and member variables  are saved into the value stack for each request and then pulled out from that stack
+	private String language;
+	
 	public String execute(){
 		//System.out.println("Hello  from execute");
 		// Better to have interface and the inject that service bean through spring
 		TutorialFinderService tutorialFinderService =  new TutorialFinderService();
-		setBestTutorialSite(tutorialFinderService.getBestTutorialSite());
+		setBestTutorialSite(tutorialFinderService.getBestTutorialSite(getLanguage()));
+		System.out.println(getLanguage());
 		return "success";
 	}
 
@@ -20,6 +25,14 @@ public class TutorialAction {
 
 	public void setBestTutorialSite(String bestTutorialSite) {
 		this.bestTutorialSite = bestTutorialSite;
+	}
+
+	public String getLanguage() {
+		return language;
+	}
+
+	public void setLanguage(String language) {
+		this.language = language;
 	}
 	
 	
--- src/org/koushik/javabrains/service/TutorialFinderService.java
+++ src/org/koushik/javabrains/service/TutorialFinderService.java
@@ -2,8 +2,12 @@ package org.koushik.javabrains.service;
 
 public class TutorialFinderService {
 	
-	public String getBestTutorialSite(){
+	public String getBestTutorialSite(String language){
+		if(language!=null && language.equals("java"))
 		return "Java Brain";
+		else{
+			return "Language not supported yet!";
+		}
 	}
 
 }
